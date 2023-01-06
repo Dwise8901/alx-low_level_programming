7a589e6cdd017f8aff9dd5ab71a2504dff3d150f
Dwise8901 <damokellymt@gmail.com>
2023-01-06 07:04:55 -0800

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: pointer
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0'; i++)
+	{
+
+	}
+	return (i);
+}
