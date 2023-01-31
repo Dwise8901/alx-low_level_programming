3aacd3d3f5995c0b0201a4d093f3c76161681ef1
Dwise8901 <damokellymt@gmail.com>
2023-01-31 07:45:56 -0800

    Print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,13 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <string.h>
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
  * for alx project
  */
@@ -33,4 +33,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif 
+#endif /* LISTS_H */
